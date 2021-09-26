8fc61c1b85b4bcd160ef1b9b413cbbdb709c8522
khaiyra <ayindekhairat@gmail.com>
2021-09-26 12:28:04 +0100

    models

--- models/traffic_data/station_summary.sql
+++ models/traffic_data/station_summary.sql
@@ -1,4 +1,6 @@
-{{ config(materialized='table') }}
+with source as (
+    select * from {{ ref('station_summary') }}
+)
 
 SELECT ID, flow_99, flow_max, flow_median, flow_total, n_obs
 FROM sensor.StationSummary
